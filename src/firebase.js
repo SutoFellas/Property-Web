8d4ea503926815c1062ec55a98aff8f3f3d49070
SutoFellas <101832795+SutoFellas@users.noreply.github.com>
2025-07-11 11:48:04 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,6 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAzcOFTStRXmgORtEywiez3ofDTzYMifp8",
-  authDomain: "property-utku.firebaseapp.com",
-  projectId: "property-utku",
-  storageBucket: "property-utku.firebasestorage.app",
-  messagingSenderId: "332430622726",
-  appId: "1:332430622726:web:0b685ad58220afd71b8387",
-  measurementId: "G-6NRYNVK94E"
-};
-
 // Firebase'i başlat
 const app = initializeApp(firebaseConfig);
 
